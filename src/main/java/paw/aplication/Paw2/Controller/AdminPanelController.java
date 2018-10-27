d73359d2a5a6c785d23cd6a5693ccaa2ce51bfc2
kacperchr <kacperchromik@gmail.com>
2018-10-27 16:36:17 +0200

    commited at 16:36

--- src/main/java/paw/aplication/Paw2/Controller/AdminPanelController.java
+++ src/main/java/paw/aplication/Paw2/Controller/AdminPanelController.java
@@ -7,8 +7,10 @@ import org.springframework.web.bind.annotation.*;
 
 import paw.aplication.Paw2.Entity.Oferta;
 import paw.aplication.Paw2.Entity.User;
+import paw.aplication.Paw2.Entity.Zamowienia;
 import paw.aplication.Paw2.Repository.OfertaRepository;
 import paw.aplication.Paw2.Repository.UserRepository;
+import paw.aplication.Paw2.Repository.ZamowieniaRepository;
 import paw.aplication.Paw2.form.LoginForm;
 import paw.aplication.Paw2.Controller.LogowanieController;
 import paw.aplication.Paw2.form.Role;
@@ -23,10 +25,15 @@ public class AdminPanelController {
 
     //LogowanieController lc = new LogowanieController();
     Role role = new Role();
-
+    @Autowired
+    private ZamowieniaRepository zamowieniaRepository;
+    //@Autowired
+    //private OfertaRepository ofertaRepository;
 
     @GetMapping(path="/adminPanel")
-    public String showAdminPanel(){
+    public String showAdminPanel(Model model){
+        model.addAttribute("data",zamowieniaRepository.findAll());
+        //model2.addAttribute("data2",ofertaRepository.findAll());
         int adminrole=role.getAdmin();
         if(adminrole==1){
             return "adminPanel";
